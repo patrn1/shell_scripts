d30622cf39af57b97ccae5113afbcb999b1955a7
patrn1 <14832593+patrn1@users.noreply.github.com>
2024-10-12 19:13:58 +0300

    Update backup_firefox.sh

--- backup_firefox.sh
+++ backup_firefox.sh
@@ -1,3 +1,15 @@
 #!/bin/bash;
 
-zip -r firefox_backup.zip ~/.mozilla/;
+current_dir=$(pwd);
+
+mkdir /tmp/firefox.tmp.dir;
+
+cd /tmp/firefox.tmp.dir;
+
+cp -r ~/.mozilla/ ./;
+
+rm -rf ./**/storage/*;
+
+zip -r firefox_backup.zip /.mozilla;
+
+cd $current_dir;
